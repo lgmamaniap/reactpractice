bd2f616e4d7df7e435a17a20eb522b0aae7c7116
gustavo mamani <lgmamaniap@gmail.com>
2023-01-10 20:17:09 -0400

    feat: boton de reset para contador

--- src/Contador.js
+++ src/Contador.js
@@ -7,12 +7,19 @@ const Contador = () => {
     if (operacion === 'suma') setValor(valor + 1)
     if (operacion === 'resta') setValor(valor - 1)
   }
+
+  const handleReset = () => {
+    setValor(0)
+  }
+  
    return (
     <>
       <h2>{valor}</h2>
       <div>
         <button onClick={() => handleCount('suma')}>Sumar</button>
         <button onClick={() => handleCount('resta')}>Restar</button>
+        <br />
+        <button onClick={handleReset}>Reset</button>
       </div>
     </>
   )
